28537e6f2d53fdda8496a2a53ff6e22de44e88c6
deoraj <deoraj.ys@gmail.com>
2019-06-06 07:56:44 +0530

    controller modified

--- RestWebServiceTest/src/main/java/com/rest/api/RestWebServiceTestApplication.java
+++ RestWebServiceTest/src/main/java/com/rest/api/RestWebServiceTestApplication.java
@@ -2,8 +2,10 @@ package com.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:event.xml")
 public class RestWebServiceTestApplication {
 
 	public static void main(String[] args) {
